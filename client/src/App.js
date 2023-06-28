0df7ac169858ef5df0d8122987357ffad9e90b25
briyeo007 <84357592+briyeo007@users.noreply.github.com>
2023-06-28 16:37:36 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { ThemeProvider } from '@mui/material/styles';
 import theme from './components/MUI-Theme/MUITheme'
 import './components/Dashboard/Dashboard'
 import Dashboard from './components/Dashboard/Dashboard';
+import LandingPage from './components/LandingPage/LandingPage';
 
 function App() {
   return (
