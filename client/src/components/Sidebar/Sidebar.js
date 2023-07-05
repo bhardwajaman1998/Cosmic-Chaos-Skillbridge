f3c0600d90b0c6ae0c8725d42bd5c2e6652ab732
Sanmeet-Singh-Dev <88464463+Sanmeet-Singh-Dev@users.noreply.github.com>
2023-07-04 23:46:03 -0700

    Updated Sidebar Icons made them link so I can test

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -6,13 +6,14 @@ import logo from '../../assets/logo.svg';
 import dashboard from '../../assets/dashboard-icon.svg';
 import courses from '../../assets/courses-icon.svg';
 import trainees from '../../assets/trainees-icon.svg';
+import { Link } from 'react-router-dom';
 
 
 const Sidebar = () => {
     const images = [
-        { title: 'Dashboard', imageUrl: dashboard },
-        { title: 'Courses', imageUrl: courses },
-        { title: 'Trainee', imageUrl: trainees },
+        { title: 'Dashboard', imageUrl: dashboard, link: '/' },
+        { title: 'Courses', imageUrl: courses, link: '/courses' },
+        { title: 'Trainee', imageUrl: trainees, link: '/employee' },
       ];
 
   return (
@@ -21,11 +22,13 @@ const Sidebar = () => {
             <img src={logo} alt="Icon" />
         </div>
         {images.map((item, index) => (
+            <Link to={item.link} key={index}>
             <SidebarIcons 
                 index={index}
                 title={item.title}
                 icon={item.imageUrl}
             />
+            </Link>
         ))}
       <DividerLine/>
         <SidebarIcons 
