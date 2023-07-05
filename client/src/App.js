1751705c2464a39c73be15624d9fcd5fa328e881
Sanmeet-Singh-Dev <88464463+Sanmeet-Singh-Dev@users.noreply.github.com>
2023-07-04 22:42:47 -0700

    Employee page add but still yet to design

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import theme from './components/MUI-Theme/MUITheme'
 import './components/Dashboard/Dashboard'
 import Dashboard from './components/Dashboard/Dashboard';
 import Home from "./pages/SamplePage/Home/Home";
+import Employee from "./pages/SamplePage/Employee/Employee";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <div className="App">
         <div>
           <Home />
+          {/* <Employee /> */}
         </div>
       </div>
     </ThemeProvider>
