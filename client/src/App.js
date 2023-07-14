29757f23a7c9bda14eedacef4946bcd67390c0fe
Aman Bhardwaj <bhardwajaman1998@gmail.com>
2023-07-13 19:33:40 -0700

    landing page fix

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,10 @@ import "./styles.scss";
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './components/MUI-Theme/MUITheme'
 import './components/Dashboard/Dashboard'
-import Dashboard from './components/Dashboard/Dashboard';
 import Home from "./pages/Home/Home";
 import Employee from "./pages/SamplePage/Employee/Employee";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from "./components/LandingPage/LandingPage";
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
       <div className="App">
         <div>
           <Routes>
+          {/* <Route path="/" element={<LandingPage />} /> */}
           <Route path="/" element={<Home />} />
           <Route path="/employee" element={<Employee />} />
           </Routes>
