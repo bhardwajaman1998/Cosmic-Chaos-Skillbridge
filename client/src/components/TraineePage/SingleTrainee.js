dc1ec9dea6f4c738310c2f865c606c8b35dcc719
Sanmeet-Singh-Dev <88464463+Sanmeet-Singh-Dev@users.noreply.github.com>
2023-07-14 03:00:45 -0700

    Updated code to display profile pic and Data Visualisation part for single trainee

--- client/src/components/TraineePage/SingleTrainee.js
+++ client/src/components/TraineePage/SingleTrainee.js
@@ -1,67 +1,87 @@
 import React, { useEffect, useState } from 'react';
 import { fetchTraineeByID } from '../../services/DashboardService';
 import CourseInformation from './CourseInformation';
+import ProfilePic from '../../assets/profile-pic-male-01.png'
+import EmployeeProgressChart from './EmployeeProgressChart';
+import TraineeDataVisual from './TraineeDataVisual';
+import ChartLabel from '../DataVisualize/ChartLabel';
 
-const SingleTrainee = ({ traineeId }) => {
-  const [selectedTrainee, setSelectedTrainee] = useState(null);
-  const [assignedPrograms, setAssignedPrograms] = useState([]);
+const SingleTrainee = ({ traineeId, traineeData }) => {
+    const [selectedTrainee, setSelectedTrainee] = useState(null);
+    const [assignedPrograms, setAssignedPrograms] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        if (traineeId) {
-          const traineeData = await fetchTraineeByID(traineeId);
-          setSelectedTrainee(traineeData);
-          setAssignedPrograms(traineeData.assigned_training_programs);
-        }
-      } catch (error) {
-        console.error('Error fetching trainee:', error);
-      }
-    };
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                if (traineeId) {
+                    const traineeData = await fetchTraineeByID(traineeId);
+                    setSelectedTrainee(traineeData);
+                    setAssignedPrograms(traineeData.assigned_training_programs);
+                }
+            } catch (error) {
+                console.error('Error fetching trainee:', error);
+            }
+        };
 
-    fetchData();
-  }, [traineeId]);
+        fetchData();
+    }, [traineeId]);
 
-  if (!selectedTrainee) {
-    return <div>Loading trainee information...</div>;
-  }
+    if (!selectedTrainee) {
+        return <div>Loading trainee information...</div>;
+    }
 
-  return (
-    <div>
-      <h1>Trainee Page</h1>
-      <div className="table-wrapper">
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Employee ID</th>
-              <th>Role</th>
-              <th>Photo</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr key={selectedTrainee._id}>
-              <td>{selectedTrainee.name}</td>
-              <td>{selectedTrainee.email}</td>
-              <td>{selectedTrainee._id}</td>
-              <td>{selectedTrainee.department.name}</td>
-              <td>
-                <img src={selectedTrainee.photo} alt="Trainee" />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <div>
-      {selectedTrainee ? (
-        <CourseInformation assignedCourses={assignedPrograms} traineeId={selectedTrainee._id} /> 
-        ) : (
-          <></>
-       )}
+    return (
+        <div>
+            <h1>Trainee Page</h1>
+            <div className="table-wrapper">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Employee ID</th>
+                            <th>Role</th>
+                            <th>Photo</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr key={selectedTrainee._id}>
+                            <td>{selectedTrainee.name}</td>
+                            <td>{selectedTrainee.email}</td>
+                            <td>{selectedTrainee._id}</td>
+                            <td>{selectedTrainee.department.name}</td>
+                            <td>
+                                <img src={ProfilePic} alt="Trainee" />
+                                {/* <img src={selectedTrainee.photo} alt="Trainee" /> */}
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div>
+                <div className="chart-container">
+                    <h3 className="chart-title">Learning Progress</h3>
+                    <div className="chart-section">
+                        {selectedTrainee ? (
+                            <EmployeeProgressChart assignedCourses={assignedPrograms} traineeId={selectedTrainee._id} />
+                        ) : (
+                            <></>
+                        )}
+                    </div>
+                </div>
+            </div>
+            <TraineeDataVisual traineeData={selectedTrainee} />
+            <div>
+                {selectedTrainee ? (
+                    <CourseInformation assignedCourses={assignedPrograms} traineeId={selectedTrainee._id} />
+                ) : (
+                    <></>
+                )}
+            </div>
+
+
         </div>
-    </div>
-  );
+    );
 };
 
 export default SingleTrainee;
