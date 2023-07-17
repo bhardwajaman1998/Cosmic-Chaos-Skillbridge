16396f75aabd2844fb68683c5aefe6c87c36bdda
Sanmeet-Singh-Dev <88464463+Sanmeet-Singh-Dev@users.noreply.github.com>
2023-07-17 00:44:36 -0700

    added Loading Spinner Component

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,71 +1,88 @@
 import React, { useEffect, useState } from 'react';
-import Sidebar from '../../components/Sidebar/Sidebar'
-import Header from '../../components/Header/Header'
-import DashboardButtons from '../../components/DashboardButtons/DashboardButtons'
-import DashboardTrainees from '../../components/DashboardTrainees/DashboardTrainees'
-import DashboardDataVisualSection from '../../components/DataVisualize/DashboardDataVisualSection'
+import Sidebar from '../../components/Sidebar/Sidebar';
+import Header from '../../components/Header/Header';
+import DashboardButtons from '../../components/DashboardButtons/DashboardButtons';
+import DashboardTrainees from '../../components/DashboardTrainees/DashboardTrainees';
+import DashboardDataVisualSection from '../../components/DataVisualize/DashboardDataVisualSection';
+import LoadingSpinner from '../../components/Loading/LoadingSpinner';
 
 import { fetchAllCourses, fetchAllTrainees, fetchTraineesByCourseId } from '../../services/DashboardService';
 
-
 const Home = () => {
+  const [loading, setLoading] = useState(true);
+  const [courses, setCourses] = useState([]);
+  const [selectedCourse, setSelectedCourse] = useState(null);
+  const [traineesData, setTraineesData] = useState([]);
 
-    const [courses, setCourses] = useState([]);
-    const [selectedCourse, setSelectedCourse] = useState(null);
-    const [traineesData, setTraineesData] = useState([]);
-  
-    useEffect(() => {
-      const getCourses = async () => {
-        try {
-          const coursesData = await fetchAllCourses();
-          setCourses(coursesData);
-          if (coursesData.length > 0) {
-            setSelectedCourse(coursesData[0])
-            console.log(selectedCourse)
-            handleCourseSelection(coursesData[0]);
-          }
-        } catch (error) {
-          // Handle error
+  useEffect(() => {
+    const getCourses = async () => {
+      try {
+        setLoading(true);
+        //remove this, this is just to simulate loading spinner
+        // await new Promise((resolve) => setTimeout(resolve, 2000));
+        const coursesData = await fetchAllCourses();
+        setCourses(coursesData);
+        if (coursesData.length > 0) {
+          setSelectedCourse(coursesData[0]);
+          handleCourseSelection(coursesData[0]);
         }
-      };
-      getCourses();
-    }, []);
-  
-    const handleCourseSelection = async (courseId) => {
-      console.log(courseId)
-        setSelectedCourse(courseId);
-        try {
-          const traineesData = await fetchTraineesByCourseId(courseId._id);
-          setTraineesData(traineesData);
-        } catch (error) {
-          // Handle error
-        }
+      } catch (error) {
+        // Handle error
+      } finally {
+        setLoading(false);
+      }
     };
-    return (
-      <React.Fragment>
-        <div className='dashboard-layout'>
-          <section>
-            <div className='layout text-2xl text-white'>
-              <div className='layout-sidebar'>
+    getCourses();
+  }, []);
+
+  const handleCourseSelection = async (courseId) => {
+    setSelectedCourse(courseId);
+    try {
+      setLoading(true);
+      // await new Promise((resolve) => setTimeout(resolve, 2000));
+      const traineesData = await fetchTraineesByCourseId(courseId._id);
+      setTraineesData(traineesData);
+    } catch (error) {
+      // Handle error
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <div className="dashboard-layout">
+        <section>
+          {loading ? (
+            <LoadingSpinner />
+          ) : (
+            <div className="layout text-2xl text-white">
+              <div className="layout-sidebar">
                 <Sidebar />
               </div>
-              <div className='layout-header'>
+              <div className="layout-header">
                 <Header />
               </div>
-              <div className='layout-main'>
+              <div className="layout-main">
                 <DashboardButtons />
               </div>
-              <div className='layout-left'>
-                <DashboardTrainees key={selectedCourse} courses={courses} traineesData={traineesData} onSelectCourse={handleCourseSelection} />
+              <div className="layout-left">
+                <DashboardTrainees
+                  key={selectedCourse}
+                  courses={courses}
+                  traineesData={traineesData}
+                  onSelectCourse={handleCourseSelection}
+                />
               </div>
-              <div className='layout-right'>
+              <div className="layout-right">
                 <DashboardDataVisualSection selectedCourse={selectedCourse} traineesData={traineesData} />
               </div>
             </div>
-          </section>
-        </div>
-      </React.Fragment>
-    );
-  };
+          )}
+        </section>
+      </div>
+    </React.Fragment>
+  );
+};
 
-export default Home
+export default Home;
