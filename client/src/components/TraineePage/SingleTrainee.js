4d332c357b2d1f4fcec052b01db61496d59b6e71
Sanmeet-Singh-Dev <88464463+Sanmeet-Singh-Dev@users.noreply.github.com>
2023-07-26 16:33:47 -0700

    Updated firebase module

--- client/src/components/TraineePage/SingleTrainee.js
+++ client/src/components/TraineePage/SingleTrainee.js
@@ -27,7 +27,7 @@ const SingleTrainee = ({ traineeId, traineeData }) => {
                     setAssignedPrograms(traineeData.assigned_training_programs);
 
                     const storageRef = firebase.storage().ref();
-                    const fileRef = storageRef.child(`trainees/${traineeId}.png`);
+                    const fileRef = storageRef.child(`trainees/${traineeId}.jpg`);
                     const downloadURL = await fileRef.getDownloadURL();
                     setImageUrl(downloadURL);
                     clearTimeout(timeout);
@@ -61,7 +61,7 @@ const SingleTrainee = ({ traineeId, traineeData }) => {
                             <LoadingSpinner />
                         ) : (
 
-                            <img src={imageUrl} alt="Trainee" />
+                            <img className="profile-pic" src={imageUrl} alt="Trainee" />
                         )}
                     </div>
                     <div className='employee-info'>
