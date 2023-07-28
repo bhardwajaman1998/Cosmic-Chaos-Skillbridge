879f2881a9b91df0df1ebf9711f98d2940875d32
briyeo007 <84357592+briyeo007@users.noreply.github.com>
2023-07-28 11:14:33 +0800

    LandingPage edit
    update landing page Desktop view - replace media and connect linkedin pages to each members profile

--- client/src/components/LandingPage/ContactCard.js
+++ client/src/components/LandingPage/ContactCard.js
@@ -3,10 +3,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import "./ContactCard.css";
 
+
+
 const ContactCard = ({ imgSrc, name, roles, linkedinUrl, githubUrl, twitterUrl }) => {
   return (
-    <div className="contact-card">
-      {imgSrc && <img src={imgSrc} alt={name} className="contact-card-img" />}
+    <div className="contact-card slide-card">
+      <img src={imgSrc} alt={name} className="contact-card-img" />
       <h2>{name}</h2>
       {roles && <p>{roles}</p>}
       <div className="social-links">
--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -8,235 +8,270 @@ import './LandingPage.css';
 import Logo from '../LandingPage/image/Logo.png';
 import image from '../LandingPage/image/image.png';
 import profile from '../LandingPage/image/Profile.png';
-import training from '../LandingPage/image/training.svg';
+import training from '../LandingPage/image/training.png';
 import Group from '../LandingPage/image/Group.png'
-import Growth from '../LandingPage/image/Growth.svg';
+import Growth from '../LandingPage/image/Growth.png';
 
+// Import the JPG images
+import AmanImage from "../LandingPage/profile/Aman.jpg";
+import BrianImage from "../LandingPage/profile/Brian.jpg";
+import MonicaImage from "../LandingPage/profile/Monica.jpg";
+import NatasjaImage from "../LandingPage/profile/Natasja.jpg";
+import SanmeetImage from "../LandingPage/profile/Sanmeet.jpg";
+import VaibhavImage from "../LandingPage/profile/Vaibhav.jpg";
+import VaneetImage from "../LandingPage/profile/Vaneet.jpg";
 
 const LandingPage = () => {
   const [cardImages] = useState([training, Group, Growth]);
   const [currentSlide, setCurrentSlide] = useState(0);
-
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   const handleSlideChange = (index) => {
     setCurrentSlide(index);
   };
 
   const sliderSettings = {
-    dots: true, // Enable dots
-    dotsClass: 'slider-dots', // Custom CSS class for the dots
+    dots: true,
+    dotsClass: 'slider-dots',
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: false,
     speed: 500,
-    
     beforeChange: (current, next) => {
       handleSlideChange(next);
     },
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
-  
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
 
   return (
     <div className='container'>
-      <header>
-        <nav className="nav">
-          <a href="/" className="site-title">
-            <img src={Logo} alt="SkillBridge" /> 
-            <span className="site-title-text">SkillBridge</span>
-            </a>            
-          <ul className="nav-list">
-            <div className='nav-work'>
+       <header>
+        <nav className={`nav ${isMobileMenuOpen ? 'mobile-menu-open' : ''}`}>
+          <div className="nav-mobile-toggle" onClick={toggleMobileMenu}>
+            <span className={`nav-mobile-icon ${isMobileMenuOpen ? 'open' : ''}`}></span>
+          </div>
+          <div className={`site-title ${isMobileMenuOpen ? 'hidden' : ''}`}>
+            <a href="/">
+              <img src={Logo} alt="SkillBridge" /> 
+              <span className="site-title-text">SkillBridge</span>
+            </a>
+          </div>
+          <ul className={`nav-list ${isMobileMenuOpen ? 'mobile-menu-open' : ''}`}>
             <li><h4><a href="/HowItWorks">How it works</a></h4></li>
             <li><h4><a href="/Testimonials">Testimonials</a></h4></li>
-            <li><h4><a href="/team">Team</a></h4></li></div>
-            <div className='signin'>
+            <li><h4><a href="/team">Team</a></h4></li>
             <li><h4><a href="/SignIn">Signin</a></h4></li>
-            <button className="btnRes-signup">
-              <a href="#Create your account">
-                <b>Sign Up</b>
-              </a>
-            </button>
-            </div>
           </ul>
+          <button className="btnRes-signup">
+            <a href="#Create your account">
+              <b>Sign Up</b>
+            </a>
+          </button>
         </nav>
       </header>
       <div className="section-container">
-      <div className='first'>
-        <h1>Empower your
-           employees to grow</h1>
-        <p>Our web app is designed to empower employees by providing them with a comprehensive platform to enhance their skills, knowledge, and overall professional development. This innovative tool harnesses the power of technology to deliver a personalized and accessible learning experience for individuals within an organization.</p>
-      
-      <div className="button-container">
-      <button className="btnRes-first">
-        <a href="#Get started now!t">
-          <b>Get started now!</b>
-        </a>
-      </button>
-      </div>
-    </div>
-    <div className="image-container">
-      <img src={image} alt="SkillBridge" className="small-image" />
-    </div>
-    </div>
-
-      <div className='second'>
-      <h2>How it works?</h2>
-      <p>The web app operates on a user-friendly platform that makes it easy for employees to navigate and access its features. Upon logging in, employees are greeted with a personalized dashboard that provides them with an overview of their learning progress, recommended courses, and upcoming training activities.</p>
-
-      <div className="card-container">
-        <Card
-          imgSrc={cardImages[0]}
-          title="Set goals for your employees"
-          details="Sed vel sem id dui rutrum porta. Morbi nec sapien sed dolor"
-          
-        >
-          
-        </Card>
-
-        <Card
-          imgSrc={cardImages[1]}
-          title="Create your courses"
-          details="Sed vel sem id dui rutrum porta. Morbi nec sapien sed dolor"
-          
-        >
-          
-        </Card>
-
-        <Card
-          imgSrc={cardImages[2]}
-          title="Measure their growth"
-          details="Sed vel sem id dui rutrum porta. Morbi nec sapien sed dolor"
-          
-        >
+        <div className="first">
+          <div className="image-container">
+        <h1>Empower your<br /> employees to grow</h1>
+    <p>Our web app is designed to empower employees by providing them with a comprehensive platform to enhance their skills, knowledge, and overall professional development. This innovative tool harnesses the power of technology to deliver a personalized and accessible learning experience for individuals within an organization.</p>
+        
   
-        </Card>
+</div>
+<img src={image} alt="SkillBridge" className="small-image" />         
+        </div>
+       
+      </div>
+      <div className="button-container">
+            <button className="btnRes-first">
+              <a href="#Get started now!">
+                <b>Get started now!</b>
+              </a>
+            </button>
+          </div>
+      <div className="second">
+        <h2>How it works?</h2>
+        <p>The web app operates on a user-friendly platform that makes it easy for employees to navigate and access its features. Upon logging in, employees are greeted with a personalized dashboard that provides them with an overview of their learning progress, recommended courses, and upcoming training activities.</p>
+
+        <Slider {...sliderSettings}>
+          <div className="card-container">
+            <Card
+              imgSrc={cardImages[0]}
+              title="Code Excellence Unleashed"
+              details="Empower your employees with cutting-edge AI tech for real-world problem-solving, code challenges, and instant, personalized feedback"
+            ></Card>
+          </div>
+
+          <div className="card-container">
+            <Card
+              imgSrc={cardImages[1]}
+              title="Customized Learning Paths"
+              details="Tailor courses with our intuitive admin interface and offer targeted lessons, 
+              industry-specific challenges for unique 
+              team needs."
+            ></Card>
+          </div>
+
+          <div className="card-container">
+            <Card
+              imgSrc={cardImages[2]}
+              title="Journey to Success"
+              details="SkillBridge offers real-time progress tracking for employees' growth. Gain valuable insights into strengths and areas for improvement."
+            ></Card>
+          </div>
+
+          
+
+        </Slider>
+        <div className="slider-dots">
+        {cardImages.map((_, index) => (
+          <div
+            key={index}
+            className={`dot ${currentSlide === index ? 'active' : ''}`}
+            onClick={() => setCurrentSlide(index)}
+          />
+        ))}
       </div>
       </div>
 
       <button className="btnRes">
-        <a href="#Get started now!t">
+        <a href="#Get started now!">
           <b>Get started now!</b>
         </a>
       </button>
 
+      <div className="slide-container">
+        <h2>What other startups are talking about us</h2>
+        <p>Across the startup community, our developer training application is generating a buzz as the go-to solution for unlocking the full potential of coding teams.</p>
 
-      <div className='slide-container'>
-  <h2>See what other startups are telling about us</h2>
-  <p>Maecenas vitae congue sapien, id molestie magna. Orci varius natoque penatibus et magnis dis parturient montes. 
-    Sed vel sem id dui rutrum porta. Morbi nec sapien sed dolor tempus tristique at a felis. Praesent eget dapibus lorem.</p>
-  
-  <div className='card-container2'>
-    <Slider {...sliderSettings}>
-      <div className="slide1 slide">
-      <img src={profile} alt="SkillBridge" />
-        <h3>"Guille"</h3>
-        <h4>Jr. Frontend Developer</h4>
-        <p>Maecenas iaculis varius pulvs.
-        "Morbi nec sapien sed dolor tempus. Vivamus non elit condimentum ante finibus gravida eget dignissim neque"</p>
-      </div>
-      <div className="slide2 slide">
-      <img src={profile} alt="SkillBridge" />
-        <h3>"Guille"</h3>
-        <h4>Jr. Frontend Developer</h4>
-        <p>"Morbi nec sapien sed dolor tempus. Vivamus non elit condimentum ante finibus gravida eget dignissim neque"</p>
-      </div>
-      <div className="slide3 slide">
-      <img src={profile} alt="SkillBridge" />
-        <h3>"Guille"</h3>
-        <h4>Jr. Frontend Developer</h4>
-        <p>"Morbi nec sapien sed dolor tempus. Vivamus non elit condimentum ante finibus gravida eget dignissim neque"</p>
-      </div>
-    </Slider>    
-  </div>
-  <div className="slider-dots">
-      {Array.from({ length: sliderSettings.slidesToShow }).map((_, index) => (
-        <div
-          key={index}
-          className={`dot ${currentSlide === index ? 'active' : ''}`}
-          onClick={() => setCurrentSlide(index)}
-        />
-      ))}
-    </div>
-</div>
+        <Slider {...sliderSettings}>
+          
+          <div className="slide1 slide">
+            <img src={profile} alt="SkillBridge" />
+            <h3>"Guille"</h3>
+            <h4>Jr. Frontend Developer</h4>
+            <p>
+              “Highly recommended! This platform has become an invaluable asset in upskilling our developer team, leading to remarkable project outcomes.”
+            </p>
+          </div>
 
-    
+          <div className="slide2 slide">
+            <img src={profile} alt="SkillBridge" />
+            <h3>"Lauren"</h3>
+            <h4>Senior Developer, Innovate Labs</h4>
+            <p>
+            “Incredible! Our startup's developers have witnessed unprecedented growth and efficiency with this game-changing training application”
+            </p>
+          </div>
 
-      <div className='teams'>
+          <div className="slide3 slide">
+            <img src={profile} alt="SkillBridge" />
+            <h3>"Jake"</h3>
+            <h4>Founder, TechVista LTD</h4>
+            <p>
+            “ A game-changer!, our startup has seen a remarkable surge in developer productivity and a substantial reduction in project delivery times”
+            </p>
+          </div>
+          
+        </Slider>
+
+        <div className="slider-dots">
+        {cardImages.map((_, index) => (
+          <div
+            key={index}
+            className={`dot ${currentSlide === index ? 'active' : ''}`}
+            onClick={() => setCurrentSlide(index)}
+          />
+        ))}
+      </div>
+      </div>
+
+      <div className="teams">
         <h2>Meet the team</h2>
         <p>At Cosmic Chaos, we take pride in our exceptional team of professionals who bring passion, creativity, and expertise to every project. Our diverse team members possess a wide range of skills and experience, allowing us to tackle various challenges and deliver outstanding results.</p>
-        <div className='member-card'>
-          <div className='card-row'>
+        
+        <div className="member-card">          
+          <div className="card-row">
+            
             <ContactCard
-              imgSrc={require('../LandingPage/profile/Aman.svg').default}
+              imgSrc={AmanImage}
               name="Aman Bhardwaj"
               roles="Full Stack Developer Lead"
-              linkedinUrl="https://www.linkedin.com/amanbhardwaj"
+              linkedinUrl="https://www.linkedin.com/in/amanbhardwaj-"
             />
 
             <ContactCard
-              imgSrc={require('../LandingPage/profile/Brian.svg').default}
+              imgSrc={BrianImage}
               name="Brian Ungjun Yeo"
               roles="Full Stack Developer"
-              linkedinUrl="https://www.linkedin.com/brianungjunyeo"
+              linkedinUrl="www.linkedin.com/in/ungjun-yeo"
             />
 
             <ContactCard
-              imgSrc={require('../LandingPage/profile/Monica.svg').default}
+              imgSrc={MonicaImage}
               name="Monica Varma"
-              roles="UX/UI Designer"              
+              roles="UX/UI Designer"
               linkedinUrl="https://www.linkedin.com/monicavarma"
             />
 
             <ContactCard
-              imgSrc={require('../LandingPage/profile/Natasja.svg').default}
+              imgSrc={NatasjaImage}
               name="Natasja Berzoini"
               roles="UX/UI Designer Lead"
-              linkedinUrl="https://www.linkedin.com/natasjaberzoini"
+              linkedinUrl="https://www.linkedin.com/in/natasjabrz/"
             />
           </div>
 
-          <div className='card-row'>
+          <div className="card-row">
             <ContactCard
-              imgSrc={require('../LandingPage/profile/Sanmeet.svg').default}
+              imgSrc={SanmeetImage}
               name="Sanmeet Singh Malli"
               roles="Full Stack Developer"
-              linkedinUrl="https://www.linkedin.com/sanmeetmalli"
+              linkedinUrl="https://www.linkedin.com/in/sanmeet-malli/"
             />
 
             <ContactCard
-              imgSrc={require('../LandingPage/profile/Vaibhav.svg').default}
+              imgSrc={VaibhavImage}
               name="Vaibhav Malhotra"
               roles="UX/UI Designer"
-              linkedinUrl="https://www.linkedin.com/vaibhavmalhotra"
+              linkedinUrl="https://www.linkedin.com/in/vaibm/"
             />
 
             <ContactCard
-              imgSrc={require('../LandingPage/profile/Vaneet.svg').default}
+              imgSrc={VaneetImage}
               name="Vaneet Kaur"
               roles="Front End"
-              linkedinUrl="https://www.linkedin.com/vaneetkaur"
-            />
+              linkedinUrl="http://linkedin.com/in/vaneetk"
+            />    
           </div>
+          
         </div>
+        
       </div>
-
       
-      <div className='final'>
-      <h2>Have Questions? Get in touch with our experts!</h2>
-      <p>If you have any questions or need further information about <strong>SkillBridge</strong> and how it can revolutionize your HR training management, we're here to help.</p>
-      <p> 
-        Reach out now and discover the power of <strong>SkillBridge</strong> for seamless training program management!</p>
+
+      <div className="final">
+        <h2>Have Questions? Get in touch with our experts!</h2>
+        <p>If you have any questions or need further information about <strong>SkillBridge</strong> and how it can revolutionize your HR training management, we're here to help.</p>
+        <p>Reach out now and discover the power of <strong>SkillBridge</strong> for seamless training program management!</p>
       </div>
       <button className="btnRes-touch">
         <b href="#Get in touch">
           Get in touch
         </b>
       </button>
-
-    
-      
     </div>
   );
 };
