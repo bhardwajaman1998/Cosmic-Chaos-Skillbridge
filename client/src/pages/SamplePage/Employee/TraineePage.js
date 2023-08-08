3c7dcd37c21337f964f23a9e1e4bb11c90fb5ba4
Aman Bhardwaj <bhardwajaman1998@gmail.com>
2023-08-07 18:54:42 -0700

    trainee page css fix and sidebar added

--- client/src/pages/SamplePage/Employee/TraineePage.js
+++ client/src/pages/SamplePage/Employee/TraineePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import TraineeInformation from '../../../components/TraineePage/TraineeInformation';
 import Header from '../../../components/Header/Header';
 import Sidebar from '../../../components/Sidebar/Sidebar';
@@ -11,20 +11,36 @@ import { useLocation } from 'react-router-dom'
 const pageTitle='Employee File'
 const TraineePage= () => {
   const location = useLocation()
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
   const { traineeId } = location.state
+  // mobile view sidebar functions
+const handleHamburgerIconClick = () => {
+  setSidebarOpen(!isSidebarOpen); // Toggle the sidebar state
+  const dashboardLayout = document.querySelector(".dashboard-layout");
+  dashboardLayout.classList.toggle("sidebar-show");
+  console.log('Hamburger icon clicked!');
+};
+
+const closeBar = () => {
+  const dashboardLayout = document.querySelector(".dashboard-layout");
+  dashboardLayout.classList.toggle("sidebar-show");
+  setSidebarOpen(false);
+}
   // if (!from) history.push('/admin');
   return (
     <React.Fragment>
       <div className='dashboard-layout'>
         <section>
           <div className='layout text-2xl text-white'>
-            <div className='layout-sidebar'>
-              <Sidebar />
+            <div className="layout-sidebar">
+              <div className={`sidebar-container ${isSidebarOpen ? 'sidebar-show' : ''}`}>
+                <Sidebar closeSidebar={closeBar}/>
+              </div>
             </div>
-            <div className='layout-header'>
-              <Header title={pageTitle} showBackButton={true} />
+            <div className="layout-header">
+              <Header showBackButton={true} onHamburgerIconClick={handleHamburgerIconClick}/>
             </div>
-            <div className='trainee-information'>
+            <div className='layout-main'>
               <SingleTrainee traineeId={traineeId}/>
             </div>
           </div>
