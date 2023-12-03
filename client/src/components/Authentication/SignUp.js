8908d485754ec70f2d3e9a8494e919b573e2998b
Aman Bhardwaj <bhardwajaman1998@gmail.com>
2023-12-02 23:07:26 -0800

    build made

--- client/src/components/Authentication/SignUp.js
+++ client/src/components/Authentication/SignUp.js
@@ -12,7 +12,7 @@ const SignUp = () => {
   const [name, setName] = useState('');
 
   const [isSignUpSuccessful, setIsSignUpSuccessful] = useState(false);
-  const API_BASE_URL = process.env.REACT_APP_LIVE_API_BASE_URL
+  const API_BASE_URL = "https://52.204.153.135:80"
 
   const handleSignUp = async () => {
     if (!email || !password || !username || !name) {
