26710a7ffaab5c69021782cbf3a5ea436078442d
Aman Bhardwaj <bhardwajaman1998@gmail.com>
2023-12-04 18:17:14 -0800

    ssl certificate configured and Base url changed

--- server/app-backend.js
+++ server/app-backend.js
@@ -1,22 +1,42 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
 const app = express();
-const port = 3000;
 const cors = require('cors');
 require('dotenv').config();
 
+const port = 3000;
+
 const path = require('path');
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
 
 require('./models/db');
 
-//import routes
-const routes = require('./routes/index') // index contains all the routes
+// Import routes
+const routes = require('./routes/index'); // index contains all the routes
 
-//use routes
+// Use routes
 app.use(routes);
-console.log(process.env.BASE_URL)
-// Start the server
-app.listen(port, () => {
+console.log(process.env.BASE_URL);
+
+// SSL/TLS certificate and private key paths
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/abhardwaj.wmdd4950.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/abhardwaj.wmdd4950.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/abhardwaj.wmdd4950.com/chain.pem', 'utf8');
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca
+};
+
+// Create an HTTPS server
+const httpsServer = https.createServer(credentials, app);
+
+// Start the HTTPS server
+httpsServer.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
+
+
