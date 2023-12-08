5f38d6452f73ed6f3c7dbb224da405fe0e9f7cff
Aman Bhardwaj <bhardwajaman1998@gmail.com>
2023-12-07 19:11:31 -0800

    endpoints updated

--- server/routes/index.js
+++ server/routes/index.js
@@ -10,12 +10,12 @@ const quizRoutes = require('./QuizRoutes')
 const quizResultRoutes = require('./QuizResultRoutes')
 
 // Register routes from other files
-router.use('/admin', adminRoutes);
-router.use('/trainee', traineeRoutes);
-router.use('/training', trainingRoutes);
-router.use('/course', courseRoutes);
-router.use('/quiz', quizRoutes);
-router.use('/quiz_result', quizResultRoutes);
+router.use('/api/admin', adminRoutes);
+router.use('/api/trainee', traineeRoutes);
+router.use('/api/training', trainingRoutes);
+router.use('/api/course', courseRoutes);
+router.use('/api/quiz', quizRoutes);
+router.use('/api/quiz_result', quizResultRoutes);
 
 // Use additional routes as needed
 module.exports = router;
