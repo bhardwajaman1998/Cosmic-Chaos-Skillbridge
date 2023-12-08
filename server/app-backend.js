6ad843fe235b1ffe8f3594847e65d47e8b6fbf1e
Aman Bhardwaj <bhardwajaman1998@gmail.com>
2023-12-07 17:20:06 -0800

    app-backend changes made

--- server/app-backend.js
+++ server/app-backend.js
@@ -20,23 +20,10 @@ const routes = require('./routes/index'); // index contains all the routes
 app.use(routes);
 console.log(process.env.BASE_URL);
 
-// SSL/TLS certificate and private key paths
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/abhardwaj.wmdd4950.com/privkey.pem');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/abhardwaj.wmdd4950.com/fullchain.pem');
-const ca = fs.readFileSync('/etc/letsencrypt/live/abhardwaj.wmdd4950.com/cert.pem');
-
-const credentials = {
-  key: privateKey,
-  cert: certificate,
-  ca: ca
-};
-
-// Create an HTTPS server
-const httpsServer = https.createServer(credentials, app);
-
-// Start the HTTPS server
-httpsServer.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
+
+
 
 
